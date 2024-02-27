5c7d8f00e1d95e3dad5fc6c8bf7b38f4d383e132
Heydar shoja <you@example.com>
2024-02-27 20:31:24 +0330

    commit add

--- src/pages/ReceiveGold.jsx
+++ src/pages/ReceiveGold.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useLocation } from 'react-router-dom';
 
+
 const handleStatusColor = (status) => {
   switch (status) {
     case "ابطال":
